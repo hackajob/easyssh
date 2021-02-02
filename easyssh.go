83f6b267fa964e0a46c781720d9ada64e5e5731e
vladiacob <vlad.iacob@hackajob.co>
2021-02-02 16:55:50 +0200

    Update easyssh.go

--- easyssh.go
+++ easyssh.go
@@ -77,6 +77,7 @@ func (ssh_conf *MakeConfig) connect() (*ssh.Session, error) {
 	config := &ssh.ClientConfig{
 		User: ssh_conf.User,
 		Auth: auths,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
 	client, err := ssh.Dial("tcp", ssh_conf.Server+":"+ssh_conf.Port, config)
